0e416c7f2bf06c92b9348616cf12bc05ae49b051
Ted J <tedmjablonowski@gmail.com>
2022-06-11 21:24:04 -0400

    Server up, comments now posting along with usernames!

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -4,7 +4,7 @@ const withAuth = require("../../utils/auth");
 
 router.post("/", withAuth, (req, res) => {
   console.log(req.body);
-  Comment.create({post_id: req.body.postId, comment_text: req.body.body, userId: req.session.userId })
+  Comment.create({post_id: req.body.postId, comment_text: req.body.body, user_id: req.session.userId })
     .then(newComment => {
       res.json(newComment);
     })
